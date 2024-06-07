62b31f61d283bd1e41e2f2499d9ba6503f74e6f6
Viacheslav Manulik <viacheslav.manulik@gmail.com>
2024-06-07 14:36:48 +0300

    Worker service added

---
+++ Gemicle.MarketerTool.Worker/Worker.cs
@@ -0,0 +1,30 @@
+namespace Gemicle.MarketerTool.Worker
+{
+    public class Worker : BackgroundService
+    {
+        private readonly ILogger<Worker> _logger;
+
+        public Worker(ILogger<Worker> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Worker started...");
+            return base.StartAsync(cancellationToken);
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                if (_logger.IsEnabled(LogLevel.Information))
+                {
+                    _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                }
+                await Task.Delay(1000, stoppingToken);
+            }
+        }
+    }
+}
